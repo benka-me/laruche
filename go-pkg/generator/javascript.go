c65df24ca78175834b529d2ab02280cbe0361c7c
benka-me <newcratie@gmail.com>
2020-04-18 13:51:24 +0200

    javascript ts-gen

--- go-pkg/generator/javascript.go
+++ go-pkg/generator/javascript.go
@@ -21,16 +21,15 @@ func (js Javascript) MainServer(bee *laruche.Bee) error {
 
 func (js Javascript) Protoc(bee *laruche.Bee) {
 	repoPath := fmt.Sprintf("%s/%s", sourcePath, bee.Repo)
-	//bin := fmt.Sprintf("%s/github.com/benka-me/hive/js-pkg/node_modules/.bin/protoc-gen-ts", sourcePath)
-	bin := "."
+	bin := fmt.Sprintf("%s/github.com/benka-me/laruche/js-pkg/node_modules/.bin/protoc-gen-ts", sourcePath)
 	jsOut := fmt.Sprintf("%s/js-pkg/src/protobuf", repoPath)
 	args := make([]string, 5)
 	args = []string{
 		fmt.Sprintf("--proto_path=%s/protobuf", repoPath),
 		fmt.Sprintf("-I=%s", sourcePath),
 		fmt.Sprintf("--plugin=protoc-gen-ts=%s", bin),
-		//fmt.Sprintf("--ts_out=service=true:%s", jsOut),
 		fmt.Sprintf("--js_out=import_style=commonjs,binary:%s", jsOut),
+		fmt.Sprintf("--ts_out=service=true:%s", jsOut),
 	}
 
 	for _, f := range bee.ProtoSetup.Files {
